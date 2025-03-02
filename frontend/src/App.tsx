1d64436df7914906b4e80b9f1a09dc3282fb343c
Ian Zhou <ian.zhou@ufl.edu>
2025-03-02 16:20:43 -0500

    removed experimental boilerplate for simple login GUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,29 +5,8 @@ import './App.css';
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [prev, setPrev] = useState(1);
-
-  const handleClick = () => {
-    setCount((current) => {
-      const nextFib = current + prev;
-      setPrev(current);
-      return nextFib;
-    });
-  };
-
   return (
     <>
-      {/* Authentication Header */}
-      <header>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -38,12 +17,12 @@ function App() {
       </div>
       <h1>GitXP</h1>
       <div className="card">
-        <button onClick={handleClick}>
-          count is {count}
-        </button>
-        <p>
-          Click for the next Fibonacci number!
-        </p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos for documentation
