c43edf8e2897453e835f63c08aa633b6c258b791
Sherif A Abdou <sherif_abdou@outlook.com>
2025-03-18 15:47:08 -0400

    Basic points algorithm and backend, still very primitive

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,43 @@
-from flask import Flask, Response, json
-from database import PointSource, User
-from sqlalchemy import Sequence, select
+from functools import reduce
+from flask import Flask, Response, json, g
+from database import PointSource, User, Repository, init_db
+from sqlalchemy import Sequence, select, Engine
+from sqlalchemy.orm import Session
+from points import calculate_points
 
-app = Flask(__name__)
+db_engine = None
+
+def create_app():
+    app = Flask(__name__)
+
+    global db_engine
+    if db_engine is None:
+        db_engine = init_db()
+            
+    return app
+
+app = create_app()
+
+
+# Query point sources for the user and the repository
+def find_point_sources_for(user, repository=None):
+    engine = db_engine
+    with Session(engine) as session:
+        stmt = select(PointSource).join(PointSource.user).join(PointSource.repo).where(User.name == user)
+        if repository is not None:
+            stmt = stmt.where(Repository.name == repository)
+
+        result = session.execute(stmt).all()
+        return result
+
+# Find all repositories that are associated with a given user
+def find_repositories_for(user):
+    engine = db_engine
+    with Session(engine) as session:
+        stmt = select(Repository).join(PointSource.user).join(PointSource.repo).where(User.name == user)
+        
+        result = session.execute(stmt).all()
+        return result
 
 @app.route("/")
 def hello_world():
@@ -10,15 +45,13 @@ def hello_world():
 
 @app.route("/users/<username>/points", methods=['GET'])
 def get_user_points(username):
-    user = select(User).where(User.name.in_([username]))
-    print(user)
+    point_sources = find_point_sources_for(username)
 
-    points = select(PointSource).join(PointSource.user).where(User.name.in_([username]))
-    print(points)
+    total_points = calculate_points(map(lambda a: a[0], point_sources))
 
     response = Response(json.dumps({
         "user": username,
-        "points": 72,
+        "points": total_points,
         }))
     response.headers["Access-Control-Allow-Origin"] = "*"
 
@@ -26,12 +59,14 @@ def get_user_points(username):
 
 @app.route("/users/<username>/repositories", methods=['GET'])
 def get_user_repositories(username):
+    repositories = find_repositories_for(username)
+    point_table = dict()
+    for (repository,) in repositories:
+        point_table[repository.name] = calculate_points(map(lambda a: a[0], find_point_sources_for(username, repository.name)))
+
     response = Response(json.dumps({
         "user": username,
-        "repositories": {
-            "imxrt-rt": 71,
-            "test": 1,
-            },
+        "repositories": point_table        
         }))
     response.headers["Access-Control-Allow-Origin"] = "*"
 
